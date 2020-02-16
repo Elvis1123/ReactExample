40dc293dc795e92dd37ec9594cdaf92a21cf069f
Elvis1123 <esehic2@etf.unsa.ba>
2020-02-16 17:37:36 +0100

    Updates

---
+++ precache-manifest.3ccc20e06790c1b4c9ff02b9cf74c911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd64d4f1d6d0e4e47ee746936615bb02",
+    "url": "/searchrobots/index.html"
+  },
+  {
+    "revision": "807b6f6dc878fcbbfa7f",
+    "url": "/searchrobots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "660a8a3f5de74d2c0e95",
+    "url": "/searchrobots/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "807b6f6dc878fcbbfa7f",
+    "url": "/searchrobots/static/js/2.48f02e3b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/searchrobots/static/js/2.48f02e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660a8a3f5de74d2c0e95",
+    "url": "/searchrobots/static/js/main.d1ce1e87.chunk.js"
+  },
+  {
+    "revision": "91a6849dce9706f804a6",
+    "url": "/searchrobots/static/js/runtime-main.afbf76b9.js"
+  }
+]);
